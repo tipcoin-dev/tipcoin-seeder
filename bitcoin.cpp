d89d2d4560178e01b82e94bfe5758a0b297efe1d
Jin Eguchi <wakiyamap@gmail.com>
2019-03-03 18:32:00 +0900

    fix string version

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -80,7 +80,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/monacoin-seeder:0.01/";
+    string ver = "/MonacoinCore:0.16.3/";
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight;
     EndMessage();
   }
